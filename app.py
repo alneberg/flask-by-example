a6dae0425a8963842b31e0eb8393111089218aae
Johannes Alneberg <johannesalneberg@gmail.com>
2015-11-10 11:32:29 +0100

    Print app environment

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
+
 @app.route('/')
 def hello():
     return "Hello World!"
@@ -14,4 +15,5 @@ def hello_name(name):
 
 
 if __name__ == '__main__':
+    print(os.environ['APP_SETTINGS'])
     app.run()
