2e0c0a52c3893739f6e800e589c750e0f96fc122
Johannes Alneberg <johannesalneberg@gmail.com>
2015-11-10 11:40:23 +0100

    Removed printing

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import Flask
 import os
 
 app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
-
 
 @app.route('/')
 def hello():
@@ -15,5 +13,4 @@ def hello_name(name):
 
 
 if __name__ == '__main__':
-    print(os.environ['APP_SETTINGS'])
     app.run()
